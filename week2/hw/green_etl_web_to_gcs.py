6569e8649fbd09c7943b3137a9f66f50830a644f
kpratchett <kiarra.pratchett@gmail.com>
2023-02-14 14:48:23 -0500

    failed push

--- week2/hw/green_etl_web_to_gcs.py
+++ week2/hw/green_etl_web_to_gcs.py
@@ -46,7 +46,7 @@ def etl_web_to_gcs() -> None:
     """The main ETL function"""
     color = "green"
     year = 2019
-    month = 4
+    month = 6
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
